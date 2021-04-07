1158679b897140928237c74a2e5aaff1d8383f92
Raphael Müller <raphaelplmuller@icloud.com>
2021-04-07 12:06:51 -0300

    First commit

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+
+export default function CommentCreate({ postId }) {
+  const [content, setContent] = useState('');
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await axios.post(`http://localhost:8001/posts/${postId}/comments`, {
+      content
+    });
+
+    setContent('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <label htmlFor='comment'>
+            New Comment
+            <input
+              id='comment'
+              value={content}
+              onChange={e => setContent(e.target.value)}
+              className='form-control'
+            />
+          </label>
+        </div>
+        <br  />
+        <button
+          type='submit'
+          className='btn btn-primary'
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+CommentCreate.defaultProps = {
+  postId: ''
+};
+
+CommentCreate.propTypes = {
+  postId: PropTypes.string
+};
+
+
---
+++ client/src/PostCreate.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default function PostCreate() {
+  const [title, setTitle] = useState('');
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await axios.post('http://localhost:8000/posts', {
+      title
+    });
+
+    setTitle('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <label htmlFor='title'>
+            Title
+            <input
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+              id='title'
+              className='form-control'
+            />
+          </label>
+        </div>
+
+        <br />
+
+        <button
+          type='submit'
+          className='btn btn-primary'
+          id='btsdsn bdsdsdsn-primary'
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
