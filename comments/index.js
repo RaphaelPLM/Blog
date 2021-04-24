97a6d678eaa67a18d5936077a0aef5031abf3411
Raphael Müller <raphaelplmuller@icloud.com>
2021-04-23 22:12:05 -0300

    Add comment moderation in front end
    Fix bugs

--- comments/index.js
+++ comments/index.js
@@ -27,18 +27,33 @@ app.post('/posts/:id/comments', async (req, res) => {
 
   await axios.post('http://localhost:4005/events', {
     type: 'CommentCreated',
-    data: { id: commentId, content, postId: req.params.id },
-    status: 'pending'
+    data: { id: commentId, content, postId: req.params.id, status: 'pending' },
   });
 
   res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
-  console.log('Received Event', req.body.type)
+app.post('/events', async (req, res) => {
+  console.log('Received Event', req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data;
+
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((comment) => comment.id === id);
+
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: { postId, id, status, content },
+    });
+  }
 
   res.send({});
-})
+});
 
 app.listen(4001, () => {
   console.log('Listening on port 4001.');
--- query/index.js
+++ query/index.js
@@ -1,35 +1,45 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const app = express()
-app.use(bodyParser.json())
-app.use(cors())
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-const posts = {}
+const posts = {};
 
 app.get('/posts', (req, res) => {
-  res.send(posts)
-})
+  res.send(posts);
+});
 
 app.post('/events', (req, res) => {
-  const { type, data } = req.body
+  const { type, data } = req.body;
+
+  console.log('Received Event', req.body.type);
 
   if (type === 'PostCreated') {
-    const { id, title } = data
+    const { id, title } = data;
 
-    posts[id] = { id, title, comments: [] }
+    posts[id] = { id, title, comments: [] };
   } else if (type === 'CommentCreated') {
-    const { id, content, postId } = data
+    const { id, content, postId, status } = data;
 
-    const post = posts[postId]
+    const post = posts[postId];
 
-    post.comments.push({ id, content })
+    post.comments.push({ id, content, status });
+  } else if (type === 'CommentUpdated') {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => comment.id === id);
+
+    comment.status = status;
+    comment.content = content;
   }
 
-  console.log(posts)
-})
+  console.log(posts);
+});
 
 app.listen(4002, () => {
-  console.log('Listening on port 4002.')
-})
+  console.log('Listening on port 4002.');
+});
